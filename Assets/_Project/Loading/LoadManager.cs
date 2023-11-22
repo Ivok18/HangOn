7d98e4895be127181a835985297af3eb0935fee6
Ivok18 <45854612+Ivok18@users.noreply.github.com>
2023-11-22 08:56:40 +0100

    (FIXED) loading system in scene "Loading" does not work on mobile brower

--- Assets/_Project/Loading/LoadManager.cs
+++ Assets/_Project/Loading/LoadManager.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,76 +8,48 @@ namespace HangOn.LoadingScene
 {
     public class LoadManager : MonoBehaviour
     {
-        [SerializeField] private GameObject loaderCanvas;
+        [SerializeField] private GameObject loadingUI;
         [SerializeField] private Image progressBar;
         [SerializeField] private float progressBarSpeed;
         [SerializeField] private int waitTimeMilliseconds;
         [SerializeField] private float progressBarTarget;
+        [SerializeField] private float timeUntilMainMenu;
 
         private void Awake()
         {
             //DOTween.SetTweensCapacity(999999, 999999);
         }
-        private void OnEnable()
+        private void Start()
         {
-            SceneManager.sceneLoaded += TryLoading;
-        }
-
-        private void OnDisable()
-        {
-            SceneManager.sceneLoaded -= TryLoading;
-        }
-
-
-        public void TryLoading(Scene scene, LoadSceneMode loadSceneMode)
-        {
-            if (scene.name == "___Loading")
-            {
-                LoadScene("__MMenu");
-            }
-        }
-
-        public async void LoadScene(string sceneName)
-        {
-            loaderCanvas.SetActive(true);
+            timeUntilMainMenu = waitTimeMilliseconds / 1000;
             progressBar.fillAmount = 0;
-
-            await Task.Delay(waitTimeMilliseconds);
-            /*
-             * why Task.Delay(waitTimeMilliseconds) ?
-             * because it seems that when don't wait, we can't see the loading process of the progress bar
-             * variable "waitTimeMilliseconds is an arbitrary value used to calibrate the amount of time we wait for the progress bar
-             * to be filled, before we actually load the data of scene "Accueil"
-            */
-
-            var scene = SceneManager.LoadSceneAsync(sceneName);
-            scene.allowSceneActivation = false;
-            do
-            {
-                await Task.Delay(100);
-                if (progressBar.fillAmount < progressBarTarget)
-                {
-                    progressBar.fillAmount = scene.progress;
-                }
-                /* why we check if fill amount is less than progress bar target ?
-                 * because when we don't, the progress bar becomes unstable (reason unknow)
-                 * why we do this just after line of code "await Task.Delay(100)" ,
-                 * honestly I don't know why myself, but it seems to work
-                 * 
-                 */
-            } while (scene.progress < progressBarTarget);
-
-            loaderCanvas.SetActive(false);
-            scene.allowSceneActivation = true;
-
         }
 
         private void Update()
         {
-            if (progressBar.fillAmount < progressBarTarget)
+            bool isLoadAlmostComplete = progressBar.fillAmount >= progressBarTarget;
+            if (!isLoadAlmostComplete)
             {
                 progressBar.fillAmount = Mathf.MoveTowards(progressBar.fillAmount, progressBarTarget, progressBarSpeed * Time.deltaTime);
             }
+            else
+            {
+                bool hasShownMainMenu = loadingUI.gameObject.activeSelf == false ? true : false;
+                if (!hasShownMainMenu)
+                {
+                    if(timeUntilMainMenu > 0)
+                    {
+                        timeUntilMainMenu -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        // we indirectly show the main menu by disabling the loading ui ..
+                        // because the loading ui is on top of the main menu in the hierarchy
+                        loadingUI.SetActive(false);
+                    }
+                }
+            }
+            
         }
     }
 }
