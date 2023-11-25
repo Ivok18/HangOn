3af35f7b46f3f61d52d7cd8d58e3ac166b2a519a
Ivok18 <45854612+Ivok18@users.noreply.github.com>
2023-11-25 12:25:06 +0100

    Added sound effects
    - menu buttons
    - button pause in scene “Game”

---
+++ Assets/_Project/Sound/SoundManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+namespace UntangleGoats.Audio
+{
+    public class SoundManager : MonoBehaviour
+    {
+        private AudioSource audioSource;
+        public static SoundManager Instance;
+        private bool canPlaySounds = true;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+        private void Update()
+        {
+            if (audioSource.isPlaying)
+                return;
+
+            audioSource.volume = 1;
+        }
+
+        public void PlaySound(AudioClip sound, float volume = 0.09f)
+        {
+            if (sound == null)
+                return;
+
+            if (!canPlaySounds)
+            {
+                Debug.Log("Cannot play sounds at the moment..");
+                return;
+            }
+
+            Debug.Log("Can play sound");
+            audioSource.volume = volume;
+            audioSource.PlayOneShot(sound);
+        }  
+
+        public void EnableSounds()
+        {
+            Debug.Log("sound effects enabled !");
+            canPlaySounds = true;
+        }
+
+        public void DisableSounds()
+        {
+            Debug.Log("sound effects disabled !");
+            canPlaySounds = false;
+        }
+    }
+}
---
+++ Assets/_Project/Sound/SoundPlayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace UntangleGoats.Audio
+{
+    public class SoundPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioClip audioClip;
+
+        public void PlaySound()
+        {
+            if(audioClip == null)
+            {
+                Debug.LogError($"error while trying to play {audioClip.name}");
+                return;
+            }
+            SoundManager.Instance.PlaySound(audioClip);
+        }
+    }
+}
