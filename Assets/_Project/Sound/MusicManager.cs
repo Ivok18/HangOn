f77e130dd90b3f9ea26f449c5c59cd7631425197
Ivok18 <45854612+Ivok18@users.noreply.github.com>
2023-11-25 12:01:42 +0100

    Added a background music

---
+++ Assets/_Project/Sound/MusicManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+namespace UntangleGoats.Audio
+{
+    public class MusicManager : MonoBehaviour
+    {
+        public static MusicManager Instance;
+        private AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+
+        public void PlayOrPauseMusic()
+        {
+            if (audioSource.clip == null)
+                return;
+
+            if (audioSource.isPlaying)
+            {
+                PauseMusic();
+            }
+            else
+            {
+                PlayMusic();
+            }
+
+
+        }
+
+        public void PlayMusic()
+        {
+            audioSource.Play();
+            Debug.Log("Playing background music..");
+        }
+
+
+        public void PauseMusic()
+        {
+            audioSource.Pause();
+            Debug.Log("Paused background music.. ");
+        }
+
+
+        public bool IsPlaying()
+        {
+            return audioSource.isPlaying == true ? true : false;
+        }
+
+        public void ChangeVolume(float newValue)
+        {
+            audioSource.volume = 1 - newValue;
+        }
+    }
+}
